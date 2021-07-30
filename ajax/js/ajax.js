1191cb3824733075364635c597c66ce01820b381
naimish1224 <naimish1224@gmail.com>
2021-07-30 14:39:43 -0400

    add canges

--- ajax/js/ajax.js
+++ ajax/js/ajax.js
@@ -3,15 +3,26 @@
 
 $(() => {
     console.log("everything is ready");
+});
 
+    const getUserById = (id) => {
+        $.getJSON(`http://localhost:8888/api/users/${id}`)
+        .done(res => {console.debug(res);
+        })
+        .fail(err => {console.error(err);
+        });
+    };
+
+    const getAllUsers = () => {
     $.getJSON(`http://localhost:8888/api/users`)
         .done(res => {
             console.debug(res);
+            display(res);
         })
         .fail(err => {
             console.error(err);
         });
-});
+    }
 
 const display = (users) => {
     let tbody = $("tbody");
